29feb180a77b9d3d8c8c4f0d39dc747e03e53a50
Nathan Harper <nathan@reflexions.co>
2019-10-01 10:33:35 -0400

    node version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const spongeMock = require('spongemock').spongeMock;
 const express = require('express');
 const app = express();
-const port = process.env.NODE_ENV == 'development' ? 3000 : 80;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     return res.send(spongeMock(req.query.text));
 })
 
-app.listen(port, () => console.log('Spongemock is running!'));
+app.listen(port, () => console.log(`Spongemock is running on port ${port}!`));
