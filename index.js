17049e3b523f8d7c18b7218db1bbdee2ea256dbc
Nathan Harper <nathan@reflexions.co>
2019-10-01 11:08:20 -0400

    body-parser added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const absurd = require('absurdify');
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 80;
 
+const app = express()
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send(absurd(req.query.text));
+    res.send(absurd(req.query.text || ''));
 })
 
 app.post('/', (req, res) => {
-    res.send(absurd(req.body.text));
+    res.send(absurd(req.body.text || ''));
 })
 
 app.listen(port, () => console.log(`Spongemock is running on port ${port}!`));
