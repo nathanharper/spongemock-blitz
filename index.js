4c1d2bfe6fa6a52dbb12bf02da072e635757d23a
Nathan Harper <nathan@reflexions.co>
2019-10-01 10:58:53 -0400

    post uses body

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
-function responder(req, res) {
-    return res.send(absurd(req.query.text));
-}
+app.get('/', (req, res) => {
+    res.send(absurd(req.query.text));
+})
 
-app.get('/', responder);
-app.post('/', responder);
+app.post('/', (req, res) => {
+    res.send(absurd(req.body.text));
+})
 
 app.listen(port, () => console.log(`Spongemock is running on port ${port}!`));
