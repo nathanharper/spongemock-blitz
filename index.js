5f8a613b872fad36bfbaaccafac42cf5c5bbc595
Nathan Harper <nathan@reflexions.co>
2019-10-01 10:40:53 -0400

    spongemock => absurdify

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const spongeMock = require('spongemock').spongeMock;
+const absurd = require('absurdify');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    return res.send(spongeMock(req.query.text));
+    return res.send(absurd(req.query.text));
 })
 
 app.listen(port, () => console.log(`Spongemock is running on port ${port}!`));
